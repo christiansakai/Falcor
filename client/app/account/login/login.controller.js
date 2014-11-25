2da0f00a40b1e1947b479c56965cd2ae32fbcfea
pat <patrick.d.mccarthy@gmail.com>
2014-11-25 15:43:27 -0500

    createStory page updated

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -15,7 +15,7 @@ angular.module('storyHubApp')
         })
         .then( function() {
           // Logged in, redirect to home
-          $location.path('/');
+          $location.path('/landing');
         })
         .catch( function(err) {
           $scope.errors.other = err.message;
--- client/app/createStory/createStory.controller.js
+++ client/app/createStory/createStory.controller.js
@@ -3,4 +3,13 @@
 angular.module('storyHubApp')
   .controller('CreatestoryCtrl', function ($scope) {
     $scope.message = 'Hello';
+
+    $scope.storyText = '';
+
+    $scope.isPrivate = false;
+
+    $scope.createStory = function(){
+    	console.log($scope.storyText)
+    }
+
   });
