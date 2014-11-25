da6d312abd34d8e1cec17254d996279b18b07252
pat <patrick.d.mccarthy@gmail.com>
2014-11-25 12:23:33 -0500

    cool landing buttons （￣ー￣）

--- client/app/landing/landing.js
+++ client/app/landing/landing.js
@@ -6,7 +6,7 @@ angular.module('storyHubApp')
       .state('landing', {
         url: '/landing',
         templateUrl: 'app/landing/landing.html',
-        controller: 'LandingCtrl as write',
-        authenticate: true
+        controller: 'LandingCtrl as write'
+        // authenticate: true
       });
   });
