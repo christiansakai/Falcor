1eb6b22b0c1d9290036f806a975e643081cc24d6
pat <patrick.d.mccarthy@gmail.com>
2014-12-09 18:19:41 -0500

    updated routing of branch charts

--- client/app/d3Keywords/d3Keywords.js
+++ client/app/d3Keywords/d3Keywords.js
@@ -7,5 +7,11 @@ angular.module('storyHubApp')
         url: '/storyKeywords',
         templateUrl: 'app/d3Keywords/d3Keywords.html',
         controller: 'D3keywordsCtrl'
+      })
+      .state('story.branchCharts.branchKeywords', {
+        url: '/branchKeywords',
+        templateUrl: 'app/d3Keywords/d3Keywords.html',
+        controller: 'D3keywordsCtrl'
       });
+
   });
--- client/app/d3Sentiment/d3Sentiment.js
+++ client/app/d3Sentiment/d3Sentiment.js
@@ -4,8 +4,13 @@ angular.module('storyHubApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('story.charts.sentiment', {
-        url: '/d3Sentiment',
+        url: '/storySentiment',
         templateUrl: 'app/d3Sentiment/d3Sentiment.html',
         controller: 'D3sentimentCtrl'
-      });
+      })
+      .state('story.branchCharts.branchSentiment', {
+        url: '/branchSentiment',
+        templateUrl: 'app/d3Sentiment/d3Sentiment.html',
+        controller: 'D3sentimentCtrl'
+      })
   });
