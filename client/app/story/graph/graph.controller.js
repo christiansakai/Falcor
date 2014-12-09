c2336b1b688cb3fa6a85efa71b77f70332b4b017
Jacques Alberts <server.jac@gmail.com>
2014-12-09 15:20:00 -0500

    Export to pdf working.

--- client/app/story/graph/graph.controller.js
+++ client/app/story/graph/graph.controller.js
@@ -407,7 +407,79 @@ function updateNode() {
 
 
 
+    // ##############################################################################
+    // <EXPORT PDF>
+    // ##############################################################################
+    $scope.exportPDF = function() {
+      console.log('Exporting PDF.');
+      
+      var doc = new jsPDF();
 
+      // We'll make our own renderer to skip this editor
+      var specialElementHandlers = {
+        '#exportBtn': function(element, renderer){
+          return true;
+        }
+      };
+      // All units are in the set measurement for the document
+      // This can be changed to "pt" (points), "mm" (Default), "cm", "in"
+      doc.fromHTML($('#storyBox').get(0), 15, 15, {
+        'width': 170, 
+        'elementHandlers': specialElementHandlers
+      });
+      //doc.save('Test.pdf');
+
+
+        doc.save('TestHTMLDoc.pdf');
+
+
+
+      // var pdf = new jsPDF('p', 'pt', 'letter'), 
+      //     source = $('#storyBox')[0]// source can be HTML-formatted string, or a reference to an actual DOM element from which the text will be scraped.
+
+      // // we support special element handlers. Register them with jQuery-style
+      // // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+      // // There is no support for any other type of selectors
+      // // (class, of compound) at this time.
+      // , 
+      // specialElementHandlers = {
+      //   // element with id of "bypass" - jQuery style selector
+      //   '#storyBox': function(element, renderer){
+      //     // true = "handled elsewhere, bypass text extraction"
+      //     return false
+      //   }
+      // }
+
+      // ,margins = {
+      //     top: 80,
+      //     bottom: 60,
+      //     left: 40,
+      //     width: 522
+      //   };
+      //   // all coords and widths are in jsPDF instance's declared units
+      //   // 'inches' in this case
+
+        
+      // pdf.fromHTML(
+      //     source // HTML string or DOM elem ref.
+      //     , margins.left // x coord
+      //     , margins.top // y coord
+      //     , {
+      //       'width': margins.width // max width of content on PDF
+      //       , 'elementHandlers': specialElementHandlers
+      //     },
+      //     function (dispose) {
+      //       // dispose: object with X, Y of the last line add to the PDF
+      //       //          this allow the insertion of new lines after html
+      //         pdf.save('Test.pdf');
+      //       },
+      //     margins
+      //   )
+    }
+    
+    // ##############################################################################
+    // </EXPORT PDF>
+    // ##############################################################################
   });
 
 // !! ABSTRACT INTO ITS OWN FILE
